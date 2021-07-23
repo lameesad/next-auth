7e23ed3a44640923ef6eb7c4103732f211680b97
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-07-23 16:15:49 +0300

    Improving Signup With Unique Email Addresses

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -18,6 +18,13 @@ async function handler(req, res) {
 
         const db = client.db()
 
+        const existingUser = await db.collection('users').findOne({ email: email })
+
+        if (existingUser) {
+            res.status(422).json({ message: 'User exists already' })
+            client.close()
+            return
+        }
         const hashedPassword = await hashPassword(password);
 
         const result = await db.collection('users').insertOne({
@@ -27,6 +34,7 @@ async function handler(req, res) {
 
 
         res.status(201).json({ message: 'Created user!' })
+        client.close()
     }
 }
 
